ed35613a9a0d48b06952016c77e4613111e2a0b2
Nour El Din Shady <rhasoldy@gmail.com>
2023-09-21 22:16:17 +0000

    Tabs to spaces

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -48,40 +48,40 @@ alias pacclean "sudo paru -Rns (paru -Qtdq)"
 # ex = EXtractor for all kinds of archives
 # usage: ex <file>
 function ex
-    if test -f $argv[1]
-        switch $argv[1]
-            case *.tar.bz2
-                tar xjf $argv[1]
-            case *.tar.gz
-                tar xzf $argv[1]
-            case *.bz2
-                bunzip2 $argv[1]
-            case *.rar
-                unrar x $argv[1]
-            case *.gz
-                gunzip $argv[1]
-            case *.tar
-                tar xf $argv[1]
-            case *.tbz2
-                tar xjf $argv[1]
-            case *.tgz
-                tar xzf $argv[1]
-            case *.zip
-                unzip $argv[1]
-            case *.Z
-                uncompress $argv[1]
-            case *.7z
-                7z x $argv[1]
-            case *.deb
-                ar x $argv[1]
-            case *.tar.xz
-                tar xf $argv[1]
-            case *.tar.zst
-                tar xf $argv[1]
-            case '*'
-                echo "'$argv[1]' cannot be extracted via ex()"
-        end
-    else
-        echo "'$argv[1]' is not a valid file"
+  if test -f $argv[1]
+    switch $argv[1]
+      case *.tar.bz2
+        tar xjf $argv[1]
+      case *.tar.gz
+        tar xzf $argv[1]
+      case *.bz2
+        bunzip2 $argv[1]
+      case *.rar
+        unrar x $argv[1]
+      case *.gz
+        gunzip $argv[1]
+      case *.tar
+        tar xf $argv[1]
+      case *.tbz2
+        tar xjf $argv[1]
+      case *.tgz
+        tar xzf $argv[1]
+      case *.zip
+        unzip $argv[1]
+      case *.Z
+        uncompress $argv[1]
+      case *.7z
+        7z x $argv[1]
+      case *.deb
+        ar x $argv[1]
+      case *.tar.xz
+        tar xf $argv[1]
+      case *.tar.zst
+        tar xf $argv[1]
+      case '*'
+        echo "'$argv[1]' cannot be extracted via ex()"
     end
+  else
+    echo "'$argv[1]' is not a valid file"
+  end
 end
---
+++ .config/fish/conf.d/variables.fish
@@ -0,0 +1,2 @@
+set -gx PATH $HOME/.config/tmux/plugins/t-smart-tmux-session-manager/bin:$PATH
+set -gx PATH $XDG_CONFIG_HOME/scripts:$PATH
