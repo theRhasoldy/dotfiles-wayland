3b6a2ea3ddc832e26b98ebcdf34b394c04ba0ff1
Nour El Din Shady <rhasoldy@gmail.com>
2023-09-26 00:17:05 +0300

    migrated dev dotfiles to its own repo

--- .config/fish/conf.d/variables.fish
+++
@@ -1,2 +0,0 @@
-set -gx PATH $HOME/.config/tmux/plugins/t-smart-tmux-session-manager/bin:$PATH
-set -gx PATH $XDG_CONFIG_HOME/scripts:$PATH
--- .config/fish/config.fish
+++
@@ -1,6 +0,0 @@
-set fish_greeting ""
-
-if status is-interactive
-  zoxide init fish | source
-  starship init fish | source
-end
